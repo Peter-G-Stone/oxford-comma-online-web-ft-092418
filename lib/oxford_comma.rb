7e3232d45428f5f5d999ec4b89f7037167ee68c0
Peter Stone <peter.stone.online@gmail.com>
2018-09-26 15:18:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,12 @@ def oxford_comma(array)
   last = nil 
   phrase = nil
   
-  if array.size > 1 
-    last = array.pop
-  end
+  if array.size == 2 
+    phrase = array.join(" and ")
+  elsif 
   
+  end 
+
   phrase = array.join(", ")
   if last
     phrase = phrase + ", and #{last}"
