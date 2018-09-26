e26b60e0b165a8fa4ce5259e5851a44a3836d12d
Peter Stone <peter.stone.online@gmail.com>
2018-09-26 15:16:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  last = nil 
+  phrase = nil
+  
+  if array.size > 1 
+    last = array.pop
+  end
+  
+  phrase = array.join(", ")
+  if last
+    phrase = phrase + ", and #{last}"
+  end 
 end
